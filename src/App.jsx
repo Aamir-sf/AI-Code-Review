4e6de013fe04c8c6a34d4493d51fdd690a65b7f4
Abu Aamir <124289209+Aamir-sf@users.noreply.github.com>
2025-12-17 12:59:17 +0530

    Fix: fresh install for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     setLoading(true);
     const response = await axios.post(
-      "https://ai-code-review-backend-three.vercel.app/ai/get-review",
+      "https://ai-code-review-backend-nu.vercel.app/ai/get-review",
       { code }
     );
 
